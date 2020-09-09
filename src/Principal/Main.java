85d0edfc01966f43bfb3394da21087689fbed467
g5-l4-frgp <mdquin@hotmail.com>
2020-09-09 20:50:52 -0300

    Primer punto

---
+++ src/Principal/Main.java
@@ -0,0 +1,12 @@
+package Principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ventalaPrincipal principal=new ventalaPrincipal();
+		principal.setVisible(true);
+
+	}
+
+}
